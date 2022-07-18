f11f02b8011de29793babd8607059f874051df48
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-07-18 12:19:44 -0600

    added bulma sass

--- awy-portfolio-app/src/components/HomePage.js
+++ awy-portfolio-app/src/components/HomePage.js
@@ -1,9 +1,16 @@
 import React from "react";
+import 'bulma/css/bulma.min.css';
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to my homepage!</h1>
-    </div>
+    <section class="hero is-info">
+      <div class="hero-body">
+        <p class="title">Andrew Young</p>
+        <p class="subtitle">Welcome to my homepage!</p>
+      </div>
+    </section>
+    // <div>
+    //   <h1>Welcome to my homepage!</h1>
+    // </div>
   );
 }
