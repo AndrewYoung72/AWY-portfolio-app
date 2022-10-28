1beee65e4ccf951f4d6337372a78b370fc5fbc5f
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-10-28 17:09:51 -0600

    portflio page work

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,97 +6,136 @@ import justParkIt from "../Images/justParkIt.png";
 import TeamDataTracker from "../Images/TeamDataTracker.png";
 import ErieTechBlog from "../Images/ErieTechBlog.png";
 import EcommerceBackend from "../Images/EcommerceBackend.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function Portfolio() {
   return (
     <div className="tile is-ancestor" id="container">
       <div className="tile is-parent">
         <article className="tile is-child notification is-info">
-          <a className="title" href="https://jesusrodriguezdev.github.io/Cocktail_Creations/">Cocktail Creations</a>
+          <a
+            className="title"
+            id="titles"
+            href="https://jesusrodriguezdev.github.io/Cocktail_Creations/"
+          >
+            Cocktail Creations
+          </a>
+
+          <figure className="image is-4by3">
+            <img src={CocktailCreations} alt="CocktailCreations"></img>
+          </figure>
           <a
             className="subtitle"
             href="https://github.com/JesusRodriguezDev/Cocktail_Creations"
           >
-            GitHub Repo
+            <span className="icon is-medium is-left">
+              <FontAwesomeIcon icon={brands("github")} />
+            </span>
           </a>
-          <figure className="image is-4by3">
-            <img src={CocktailCreations} alt="CocktailCreations" ></img>
-          </figure>
         </article>
       </div>
 
       <div className="tile is-parent">
         <article className="tile is-child notification is-info">
-          <p className="title">Just Park It</p>
           <a
-            className="subtitle"
-            href="https://github.com/Noah8863/Just-Park-It"
+            className="title"
+            id="titles"
+            href="https://github.com/Noah8863/Just-Park-It#readme"
           >
-            GitHub Repo
+            Just Park It
           </a>
           <figure className="image is-4by3">
             <img src={justParkIt} alt="Project 2"></img>
           </figure>
+          <a
+            className="subtitle"
+            href="https://github.com/Noah8863/Just-Park-It"
+          >
+            <span className="icon is-medium is-left">
+              <FontAwesomeIcon icon={brands("github")} />
+            </span>
+          </a>
         </article>
       </div>
 
       <div className="tile is-parent">
         <article className="tile is-child notification is-info">
-          <p className="title">Weather Dashboard</p>
           <a
-            className="subtitle"
-            href="https://github.com/AndrewYoung72/06-weather-dashboard-AWY"
+            className="title"
+            id="titles"
+            href="https://andrewyoung72.github.io/06-weather-dashboard-AWY/"
           >
-            GitHub Repo
+            Weather Dashboard
           </a>
           <figure className="image is-4by3">
             <img src={WeatherDashboard} alt="Weather Dashboard"></img>
           </figure>
+          <a
+            className="subtitle"
+            href="https://github.com/AndrewYoung72/06-weather-dashboard-AWY"
+          >
+            <span className="icon is-medium is-left">
+              <FontAwesomeIcon icon={brands("github")} />
+            </span>
+          </a>
         </article>
       </div>
 
       <div className="tile is-parent">
         <article className="tile is-child notification is-info">
-          <p className="title">Erie Tech Blog</p>
+          <p className="title" id="titles">
+            Erie Tech Blog
+          </p>
+          <figure className="image is-4by3">
+            <img src={ErieTechBlog} alt="Erie Tech Blog"></img>
+          </figure>
           <a
             className="subtitle"
             href="https://intense-ridge-37443.herokuapp.com/"
           >
-            GitHub Repo
+            <span className="icon is-medium is-left">
+              <FontAwesomeIcon icon={brands("github")} />
+            </span>
           </a>
-          <figure className="image is-4by3">
-            <img src={ErieTechBlog} alt="Erie Tech Blog"></img>
-          </figure>
         </article>
       </div>
 
       <div className="tile is-parent">
         <article className="tile is-child notification is-info">
-          <p className="title">E-Commerce-Backend</p>
+          <p className="title" id="titles">
+            E-Commerce-Backend
+          </p>
+          <figure className="image is-4by3">
+            <img src={EcommerceBackend} alt=""></img>
+          </figure>
           <a
             className="subtitle"
             href="https://github.com/AndrewYoung72/sequalize-e-commerce-back-end"
           >
-            GitHub Repo
+            <span className="icon is-medium is-left">
+              <FontAwesomeIcon icon={brands("github")} />
+            </span>
           </a>
-          <figure className="image is-4by3">
-            <img src={EcommerceBackend} alt=""></img>
-          </figure>
         </article>
       </div>
 
       <div className="tile is-parent">
         <article className="tile is-child notification is-info">
-          <p className="title">Team Data Tracker</p>
+          <p className="title" id="titles">
+            Team Data Tracker
+          </p>
+          <figure className="image is-4by3">
+            <img src={TeamDataTracker} alt=""></img>
+          </figure>
           <a
             className="subtitle"
             href="https://github.com/AndrewYoung72/Team-data-tracker"
           >
-            GitHub Repo
+            <span className="icon is-medium is-left">
+              <FontAwesomeIcon icon={brands("github")} />
+            </span>
           </a>
-          <figure className="image is-4by3">
-            <img src={TeamDataTracker} alt=""></img>
-          </figure>
         </article>
       </div>
     </div>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "bulma/css/bulma.min.css";
-import resume from "../Images/resume.png"
+import resume from "../Images/resume.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function Resume() {
   return (
     <div className="container is-fluid">
       <div className="notification is-info" id="resume-ctr">
         <a clasName="button is-hovered" href="https://resume.io/r/wBcWFPraV">
-          <button className="button ">Download My Resume</button>
+          <span>
+            <FontAwesomeIcon
+              icon={solid("download")}
+              className="icon is-medium"
+              id="download"
+            />
+          </span>
         </a>
         <figure className="image is-4by3">
           <img src={resume} alt="my resume"></img>
